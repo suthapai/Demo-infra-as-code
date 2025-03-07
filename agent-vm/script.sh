9edc2281cba280cf8da1f979b0e9f91aa2d633e1
Sujetha Pai Krishna Pai <122850668+suthapai@users.noreply.github.com>
2025-03-07 09:09:50 -0500

    Update script.sh

--- agent-vm/script.sh
+++ agent-vm/script.sh
@@ -33,7 +33,7 @@ tar zxvf vsts-agent-linux-x64.tar.gz -C myagent
 chmod -R 777 myagent
 # Configuration of the self-hosted agent
 cd myagent
-./config.sh --unattended --url https://dev.azure.com/Learnazure162022 --auth pat --token pibvctgpmeutdwsy7vfypwerewewewewewewecnf5ixknxgtwpcbiqn6eg5mlrchq --pool Default --agent aksagent --acceptTeeEula
+./config.sh --unattended --url https://dev.azure.com/sujethapai --auth pat --token 4yntpK7riMFJ1jZHnjBdPZFAligUd2EJGshHltRDpLEkaykJBhZ8JQQJ99BCACAAAAAAAAAAAAASAZDOLX6Q --pool Default --agent aksagent --acceptTeeEula
 # Start the agent service
 sudo ./svc.sh install
 sudo ./svc.sh start
